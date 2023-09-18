ac38a5f8518aebbe9e7c63539e2915abb09bfc9a
gooseman789 <106124000+gooseman789@users.noreply.github.com>
2023-09-17 21:14:58 -0400

    HTMl CSS and javascript basics added

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+var correctEL = document.querySelectorAll(".correct");
+var wrongEL = document.querySelectorAll(".wrong");
+var startEL = document.querySelector("#start");
+
+function nextplus() {
+
+}
+
+function nextstay() {
+
+}
+
+function startgame() {
+
+}
+
+startEL.addEventListener("click", startgame)
+wrongEL.addEventListener("click", nextstay)
+correctEL.addEventListener("click", nextplus)
