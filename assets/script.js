8c638a20fe3d3a12d007f6424683a5ff5857e791
unknown <j.a.gotheridge@gmail.com>
2023-09-20 09:11:25 -0400

    testing

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 var correctEL = document.querySelectorAll(".correct");
 var wrongEL = document.querySelectorAll(".wrong");
 var startEL = document.querySelector("#start");
+var timerEL = document.querySelector('#timer');
 var question0EL = document.querySelector('#question0');
 var question1EL = document.querySelector('#question1');
 var question2EL = document.querySelector('#question2');
@@ -29,17 +30,30 @@ var cardsEL = [
     question11EL,
 ]
 
-function nextplus() {
-
+function countdown() {
+    var timeLeft = 60;
+    var timeInterval = setInterval(function() {
+        timeLeft--;
+        timeInterval.textContent = "Time left: " + timeLeft + " seconds"
+        if (timeLeft === 0) {
+            clearInterval(timeInterval);
+        }
+    }, 1000);
 }
 
-function nextstay() {
-    //
+function nextplus(event) {
+    event.preventDefault();
+}
+
+function nextstay(event) {
+    event.preventDefault();
     currentEL = document.querySelector("#show");
    // currentEL.removeAttribute('id');
 }
 
-function startgame() {
+function startgame(event) {
+    event.preventDefault();
+    countdown();
     nextstay();
 }
 
@@ -50,3 +64,5 @@ wrongEL.forEach((el, i) => {
 correctEL.forEach((el,i) => {
     el.addEventListener('click', () => alert("clicked correct"))
 })
+
+//this is a test to see if this actually worked.
