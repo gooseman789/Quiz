667fc86d65b2c7ed21cdf8b8b8a2e86f70616d8c
unknown <j.a.gotheridge@gmail.com>
2023-09-21 12:21:50 -0400

    added functions to buttons and added HTML missing pieces

--- assets/script.js
+++ assets/script.js
@@ -2,66 +2,47 @@ var correctEL = document.querySelectorAll(".correct");
 var wrongEL = document.querySelectorAll(".wrong");
 var startEL = document.querySelector("#start");
 var timerEL = document.querySelector('#timer');
-var question0EL = document.querySelector('#question0');
-var question1EL = document.querySelector('#question1');
-var question2EL = document.querySelector('#question2');
-var question3EL = document.querySelector('#question3');
-var question4EL = document.querySelector('#question4');
-var question5EL = document.querySelector('#question5');
-var question6EL = document.querySelector('#question6');
-var question7EL = document.querySelector('#question7');
-var question8EL = document.querySelector('#question8');
-var question9EL = document.querySelector('#question9');
-var question10EL = document.querySelector('#question10');
-var question11EL = document.querySelector('#question11');
+var cardsEL = document.querySelectorAll('.questions');
+var HighScoreBut = document.querySelector('#highscore');
+var scoreEL = document.querySelector('#score');
+var restartBut = document.querySelector('#restart');
+var resetBut  = document.querySelector('#reset');
 
-var cardsEL = [
-    question0EL,
-    question1EL,
-    question2EL,
-    question3EL,
-    question4EL,
-    question5EL,
-    question6EL,
-    question7EL,
-    question8EL,
-    question9EL,
-    question10EL,
-    question11EL,
-]
+
+
+scoreEL.textContent = "Your score was " + scoreNum;
+
+var scoreNum = "0";
 
 function countdown() {
     var timeLeft = 60;
     var timeInterval = setInterval(function() {
         timeLeft--;
-
-        timeInterval.textContent = "Time left: " + timeLeft + " seconds"
-        if (timeLeft === 0) {
         timerEL.textContent = "Time left: " + timeLeft + " seconds"
         if (timeLeft <= 0) {
             clearInterval(timeInterval);
+            currentEL = document.querySelector("#show");
+            cardsEL[cardsEL.length-1].setAttribute('id','show');
+            currentEL.removeAttribute('id');
         }
-    }
     }, 1000);
-    console.log("testtime")
 }
 
-
-function nextplus(event) {
-    event.preventDefault();
+function nextplus() {
 }
-
 
 function nextstay() {
     currentEL = document.querySelector("#show");
-    console.log("test1")
-    cardsEL.forEach((el, i) => {
-        if (el == currentEL) {
-            //cardsEL[i++].setAttribute('id', 'show');
-            console.log("test2")
-        };
-    });
-    currentEL.removeAttribute('id');
+    console.log("test1");
+    for (let i=0; i < cardsEL.length; i++) {
+        if (cardsEL[i] === currentEL) {
+            console.log(cardsEL[i]);
+            cardsEL[i+1].setAttribute('id', 'show');
+            currentEL.removeAttribute('id');
+            break
+        }
+    }
+    
 }
 
 function startgame() {
@@ -69,13 +50,39 @@ function startgame() {
     nextstay();
     console.log("test");
 }
+//starts game
+startEL.addEventListener("click", startgame);
 
-startEL.addEventListener("click", startgame)
+//move to next card, lower time
 wrongEL.forEach((el, i) => {
-    el.addEventListener('click', () => alert('clicked loss'))
-})
+el.addEventListener('click', function() {
+    currentEL = document.querySelector("#show");
+    console.log("test1");
+    for (let i=0; i < cardsEL.length; i++) {
+        if (cardsEL[i] === currentEL) {
+            cardsEL[i+1].setAttribute('id', 'show');
+            currentEL.removeAttribute('id');
+            break
+        }
+    }
+    
+})});
+
+//move to next card, add one to score
 correctEL.forEach((el,i) => {
-    el.addEventListener('click', () => alert("clicked correct"))
+    el.addEventListener('click', nextplus())});
+
+
+//High score button to move to final card
+HighScoreBut.addEventListener('click', function() {
+    currentEL = document.querySelector("#show");
+    cardsEL[cardsEL.length-1].setAttribute('id','show');
+    currentEL.removeAttribute('id');
 })
 
-//this is a test to see if this actually worked.
+//puts you back at the first question
+restartBut.addEventListener('click', function() {
+    cardsEL[cardsEL.length-1].removeAttribute('id');
+    cardsEL[1].setAttribute('id', 'show');
+    countdown();
+})
